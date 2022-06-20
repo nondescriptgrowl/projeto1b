b0a550a47e56e46d2ea4c4814b13088bd8d0fd8f
predo <pedro.silva1154@etec.sp.gov.br>
2022-06-20 20:08:39 -0300

    Criação Pasta Pessoa'

--- projeto1b/Program.cs
+++ projeto1b/Program.cs
@@ -10,6 +10,15 @@ namespace projeto1b
     {
         static void Main(string[] args)
         {
+            int numero;
+            Pessoa aluno;
+            aluno = new Pessoa();
+            Pessoa professor = new Pessoa();
+            aluno.nome = "Pedro";
+            professor.nome = "Prof. Carvalho";
+            Console.WriteLine("Aluno:" + aluno.nome);
+            Console.WriteLine("Professor:" + professor.nome);
+            Console.ReadKey();
         }
     }
 }
